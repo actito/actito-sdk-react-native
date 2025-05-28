2052c4189bd2517577c968565077937104afdb95
yevheniismirnov <yevhenii.smirnov@outlook.com>
2025-05-28 11:02:23 +0100

    feat(sample): add app

---
+++ packages/sample/index.js
@@ -0,0 +1,46 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import { App } from './src/app';
+import { name as appName } from './app.json';
+// import { ActitoGeo } from 'react-native-actito-geo';
+// import {
+//   BackgroundCallbackBeaconEntered,
+//   BackgroundCallbackBeaconExited,
+//   BackgroundCallbackBeaconsRanged,
+//   BackgroundCallbackLocationUpdated,
+//   BackgroundCallbackRegionEntered,
+//   BackgroundCallbackRegionExited,
+// } from './src/background/background-callback';
+
+// function setupBackgroundCallbacks() {
+//   ActitoGeo.setLocationUpdatedBackgroundCallback(
+//     BackgroundCallbackLocationUpdated
+//   );
+//
+//   ActitoGeo.setRegionEnteredBackgroundCallback(
+//     BackgroundCallbackRegionEntered
+//   );
+//
+//   ActitoGeo.setRegionExitedBackgroundCallback(
+//     BackgroundCallbackRegionExited
+//   );
+//
+//   ActitoGeo.setBeaconEnteredBackgroundCallback(
+//     BackgroundCallbackBeaconEntered
+//   );
+//
+//   ActitoGeo.setBeaconExitedBackgroundCallback(
+//     BackgroundCallbackBeaconExited
+//   );
+//
+//   ActitoGeo.setBeaconsRangedBackgroundCallback(
+//     BackgroundCallbackBeaconsRanged
+//   );
+// }
+
+// setupBackgroundCallbacks();
+
+AppRegistry.registerComponent(appName, () => App);
---
+++ packages/sample/src/background/background-callback.tsx
@@ -0,0 +1,43 @@
+// import {
+//   ActitoBeacon,
+//   ActitoLocation,
+//   ActitoRegion,
+// } from 'react-native-actito-geo';
+// import { logCustomBackgroundEvent } from './background-logger';
+//
+// export const BackgroundCallbackLocationUpdated = async (
+//   location: ActitoLocation
+// ) => {
+//   await logCustomBackgroundEvent('LocationUpdated', location);
+// };
+//
+// export const BackgroundCallbackRegionEntered = async (
+//   region: ActitoRegion
+// ) => {
+//   await logCustomBackgroundEvent('RegionEntered', region);
+// };
+//
+// export const BackgroundCallbackRegionExited = async (
+//   region: ActitoRegion
+// ) => {
+//   await logCustomBackgroundEvent('RegionExited', region);
+// };
+//
+// export const BackgroundCallbackBeaconEntered = async (
+//   beacon: ActitoBeacon
+// ) => {
+//   await logCustomBackgroundEvent('BeaconEntered', beacon);
+// };
+//
+// export const BackgroundCallbackBeaconExited = async (
+//   beacon: ActitoBeacon
+// ) => {
+//   await logCustomBackgroundEvent('BeaconExited', beacon);
+// };
+//
+// export const BackgroundCallbackBeaconsRanged = async (data: {
+//   region: ActitoRegion;
+//   beacons: ActitoBeacon[];
+// }) => {
+//   await logCustomBackgroundEvent('BeaconsRanged', data);
+// };
